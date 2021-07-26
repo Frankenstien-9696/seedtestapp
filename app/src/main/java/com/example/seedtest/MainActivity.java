a4f6eaf979891d9275a2485ad0e63fbd73d6eb09
Frankenstien-9696 <alpaca05>
2021-07-26 11:20:26 +0500

    add image from camera

--- app/src/main/java/com/example/seedtest/MainActivity.java
+++ app/src/main/java/com/example/seedtest/MainActivity.java
@@ -85,6 +85,15 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        cam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,checkfromcam.class);
+
+                startActivity(intent);
+
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/seedtest/checkfromcam.java
+++ app/src/main/java/com/example/seedtest/checkfromcam.java
@@ -5,75 +5,75 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.jetbrains.annotations.NotNull;
 
-public class checkfromgallery extends AppCompatActivity {
+public class checkfromcam extends AppCompatActivity {
+    private static final int PERMISSIONCODE=1002;
+    private static final int IMAGE_CAPTURE_CODE=1003;
+    Uri imageuri;
     ImageView img;
     TextView answer;
-    //View colorview;
-
     Bitmap bitmap;
     int total=0, count=0;
     float percent;
-    private static final int IMAGE_PICK_CODE=1000;
-    private static final int PERMISSION_CODE=1001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_checkfromgallery);
-
-
-        img=(ImageView) findViewById(R.id.imageView2);
-        answer=(TextView) findViewById(R.id.show2);
-
-
-
-                if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
-                    if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)==(PackageManager.PERMISSION_DENIED)){
-                        //permission not granted then request it
-                        String[] permissions={Manifest.permission.READ_EXTERNAL_STORAGE};
-                        //runtime permission
-                        requestPermissions(permissions, PERMISSION_CODE);
-                    }
-                    else{
-                        //permission already granted
-                        pickImageFromGallery();
-                    }
-                }
-                else{
-                    //system OS is earlier than marshmallow
-                    pickImageFromGallery();
-                }
+        setContentView(R.layout.activity_checkfromcam);
+        img=(ImageView)findViewById(R.id.imageView2);
+        answer=(TextView)findViewById(R.id.show2);
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            if(checkSelfPermission(Manifest.permission.CAMERA)== PackageManager.PERMISSION_DENIED ||
+            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                String[] permission= {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permission,PERMISSIONCODE);
+            }
+            else{
+                openCamera();
 
             }
-
-    private void pickImageFromGallery() {
-        //intent to pick image
-        Intent intent=new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent, IMAGE_PICK_CODE);
+        }
+        else{
+                openCamera();
+        }
+       
     }
-    //handle result at runtime permission
+
+    private void openCamera() {
+        ContentValues values= new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE,"New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION,"From the Camera");
+        imageuri= getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+        Intent cameraIntent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageuri);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch(requestCode){
-            case PERMISSION_CODE:{
+        switch (requestCode){
+            case PERMISSIONCODE:{
                 if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
                     //permission granted
-                    pickImageFromGallery();
+                    openCamera();
                 }
                 else{
                     //permission not granted
@@ -86,14 +86,15 @@ public class checkfromgallery extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode== RESULT_OK && requestCode==IMAGE_PICK_CODE){
-            img.setImageURI(data.getData());
+        if(resultCode==RESULT_OK){
+            img.setImageURI(imageuri);
             BitmapDrawable abmp = (BitmapDrawable)img.getDrawable();
             bitmap= abmp.getBitmap();
             bitmap=extract(bitmap);
             img.setImageBitmap(bitmap);
         }
     }
+
     public Bitmap extract(Bitmap bi) {
         final double GS_RED = 0.299;
         final double GS_GREEN = 0.587;
